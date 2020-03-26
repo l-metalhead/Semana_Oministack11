9096834856e531de1dd3baa7d4084f0c31d5e06f
Lucas Soares Nogueira <lucasn.metalhead@gmail.com>
2020-03-26 17:14:19 +0000

    Adiciona dia 01 e knex do dia 02 da semana 11 omnistack

---
+++ SemanaOmnistack11/aulas/backend/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.post('/users', (request, response) => {
+    const body = request.body;
+
+    console.log(body);
+
+    return response.json({
+        evento: 'Semana Omnistack 11.0',
+        aluno: 'Lucas Nogueira'
+    });
+});
+
+app.listen(3333);
